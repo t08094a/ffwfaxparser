ccfec5fa7177f954cf68c016355cac3e54f7afc6
t08094a <jochen.faehnlein@ff-ipsheim.de>
2015-11-11 18:55:35 +0100

    single test executables for each test class

--- tests/IlsAnsbachTest.cpp
+++ tests/IlsAnsbachTest.cpp
@@ -6,12 +6,27 @@
 
 using namespace boost::unit_test;
 
-BOOST_AUTO_TEST_CASE( ParseFile )
+BOOST_AUTO_TEST_CASE( ParseFileWithEmptyFilename )
 {
    IlsAnsbach* parser = new IlsAnsbach();
-   bool result = parser->Parse("");
+   IOperation* result = parser->Parse("");
 
-   BOOST_CHECK(true == result);
+   BOOST_CHECK(result == nullptr);
 
    delete parser;
+}
+
+BOOST_AUTO_TEST_CASE( ParseFileWithNotExistingFile )
+{
+   IlsAnsbach* parser = new IlsAnsbach();
+   IOperation* result = parser->Parse("/tmp/sowieso");
+
+   BOOST_CHECK(result == nullptr);
+
+   delete parser;
+}
+
+BOOST_AUTO_TEST_CASE( Fail )
+{
+   BOOST_CHECK(true == false);
 }
